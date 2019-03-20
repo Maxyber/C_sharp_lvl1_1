7e152235506e0645e9be298bd1cf85f3780901c7
Maxyber <ghostpaladin@mail.ru>
2019-03-20 19:59:55 +0300

    Доделал домашнее задание к вебинару №1. Все задачи выполнены, включая со звездочков

---
+++ Lesson_1/ClassConsoleInOut/ClassConsole.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassConsoleInOut
+{
+    public class ClassConsole
+    {
+        static public void ConsolePause()
+        {
+            Console.ReadKey();
+        }
+
+        static public void ConsolePrint(string str)
+        {
+            Console.WriteLine(str);
+            Console.ReadKey();
+        }
+    }
+}
