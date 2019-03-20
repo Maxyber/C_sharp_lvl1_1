38a4f407c1fe3b58f98408d23a2bf219bd2453d2
maxyber <ghostpaladin@mail.ru>
2019-03-20 16:57:59 +0300

    выполнены 1-5 задания, необходимо доделать только последнее.

--- Lesson_1/Homework/Program.cs
+++ Lesson_1/Homework/Program.cs
@@ -22,12 +22,20 @@ namespace Homework
              
              
         */
+        // метод расчета расстояния между точками
         static double PointsDistanceMethod(int x1, int y1, int x2, int y2)
         {
             double distance;
             distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
             return distance;
         }
+        // метод вывода строки по требуемым координатам консоли
+        static void PrintMsg (int x, int y, string pmsg)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine(pmsg);
+        }
+
         static void Main(string[] args)
         {
             string Family = "";
@@ -37,7 +45,7 @@ namespace Homework
             int Weight;
 
             //  программа Анкета
-            Console.WriteLine("Введите вашу фамилию"); 
+            Console.WriteLine("Введите вашу фамилию");
             Family = Console.ReadLine();
 
             Console.WriteLine("Введите ваше имя");
@@ -64,7 +72,7 @@ namespace Homework
             // программа рассчета индекса массы тела
             Height = Height / 100; // приводим рост человека к значению в метрах для расчета показателя
             double IMT;
-            IMT = (Weight)/(Math.Pow(Height, 2));
+            IMT = (Weight) / (Math.Pow(Height, 2));
             Console.WriteLine("Ваш индекс массы тела: {0:F2}", IMT);
             Console.ReadKey();
 
@@ -81,13 +89,74 @@ namespace Homework
             y2 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine($"x1 - {x1}, y1 = {y1}, x2 - {x2}, y2 - {y2}");
 
-            Console.WriteLine($"Расстояние между точкам: {PointsDistanceMethod(x1,y1,x2,y2):f2}");
+            Console.WriteLine($"Расстояние между точкам: {PointsDistanceMethod(x1, y1, x2, y2):f2}");
 
             /*
             Написать программу обмена значениями двух переменных:
             а) с использованием третьей переменной;
 	        б) *без использования третьей переменной.
             */
+
+            string strX, strY; // переменные для которых требуется обмен значениями
+            int XX, YY;
+
+            Console.WriteLine("Введите значение первой переменной x");
+            strX = Console.ReadLine();
+            Console.WriteLine("Введите значение второй переменной y");
+            strY = Console.ReadLine();
+            // обмен значениями переменных с использованием третьей любой тип
+            /*
+            strTemp = strY;
+            strY = strX;
+            strX = strTemp;
+            */
+            // обмен значениями переменных без использования третьей числовой тип
+            XX = Convert.ToInt32(strX);
+            YY = Convert.ToInt32(strY);
+            XX = XX + YY;
+            YY = XX - YY;
+            XX = XX - YY;
+            Console.WriteLine($"X = {XX}, Y = {YY}");
+
+
+            /*
+            а) Написать программу, которая выводит на экран ваше имя, фамилию и город проживания.
+            б) *Сделать задание, только вывод организовать в центре экрана.
+            в) **Сделать задание б с использованием собственных методов (например, Print(string ms, int x,int y).
+            */
+            Console.Clear();
+            string City = "Балашиха", FIO;
+            int consoleWidth, consoleHeight;
+            double xStart, yStart;
+
+            consoleHeight = Console.WindowHeight;
+            consoleWidth = Console.WindowWidth;
+            Console.WriteLine("Введите название города в котором живете");
+            City = Console.ReadLine();
+            Console.WriteLine("Введите ФИО, которое хотите отобразить");
+            FIO = Console.ReadLine();
+
+            // рассчет стартовых координат вывода первой строки сообщения
+            xStart = (consoleWidth - FIO.Length) / 2;
+            yStart = consoleHeight / 2;
+            consoleWidth = Convert.ToInt32(Math.Floor(xStart));
+            consoleHeight = Convert.ToInt32(yStart) - 1;
+            
+            PrintMsg(consoleWidth, consoleHeight, FIO); // вывод первой строки сообщения
+
+            // рассчет стартовых координат вывода второй строчки сообщения
+            consoleWidth = Console.WindowWidth;
+            xStart = (consoleWidth - City.Length) / 2;
+            consoleWidth = Convert.ToInt32(Math.Floor(xStart));
+            consoleHeight = consoleHeight + 1;
+
+            PrintMsg(consoleWidth, consoleHeight, City); // вывод второй строки сообщения
+
+            /*
+            *Создать класс с методами, которые могут пригодиться в вашей учебе(Print, Pause).
+            */
+
+
 
             Console.ReadKey();
         }
