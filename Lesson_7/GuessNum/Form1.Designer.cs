d07363a235fa0d3b58b61d04a071fc2d1b9fd44b
maxyber <ghostpaladin@mail.ru>
2019-04-10 15:19:20 +0300

    Начал выполнение задания 7.2

---
+++ Lesson_7/GuessNum/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace GuessNum
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.start = new System.Windows.Forms.Button();
+            this.number = new System.Windows.Forms.Label();
+            this.lblTry = new System.Windows.Forms.Label();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.lblPlayer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(194, 10);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(105, 25);
+            this.start.TabIndex = 0;
+            this.start.Text = "Start game";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // number
+            // 
+            this.number.AutoSize = true;
+            this.number.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.number.Location = new System.Drawing.Point(10, 10);
+            this.number.Name = "number";
+            this.number.Size = new System.Drawing.Size(171, 15);
+            this.number.TabIndex = 1;
+            this.number.Text = "Ваше последнее число 0";
+            this.number.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblTry
+            // 
+            this.lblTry.AutoSize = true;
+            this.lblTry.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblTry.Location = new System.Drawing.Point(13, 46);
+            this.lblTry.Name = "lblTry";
+            this.lblTry.Size = new System.Drawing.Size(81, 15);
+            this.lblTry.TabIndex = 2;
+            this.lblTry.Text = "Попытка №0";
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblInfo.Location = new System.Drawing.Point(16, 101);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(205, 13);
+            this.lblInfo.TabIndex = 3;
+            this.lblInfo.Text = "Вас приветствует игра \"Угадай число\"";
+            // 
+            // lblPlayer
+            // 
+            this.lblPlayer.AutoSize = true;
+            this.lblPlayer.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblPlayer.Location = new System.Drawing.Point(16, 71);
+            this.lblPlayer.Name = "lblPlayer";
+            this.lblPlayer.Size = new System.Drawing.Size(81, 15);
+            this.lblPlayer.TabIndex = 4;
+            this.lblPlayer.Text = "Ход игрока 1";
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(311, 160);
+            this.Controls.Add(this.lblPlayer);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.lblTry);
+            this.Controls.Add(this.number);
+            this.Controls.Add(this.start);
+            this.Name = "mainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Угадай число";
+            this.Load += new System.EventHandler(this.mainForm_Load);
+            this.SizeChanged += new System.EventHandler(this.mainForm_SizeChanged);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Label number;
+        private System.Windows.Forms.Label lblTry;
+        private System.Windows.Forms.Label lblInfo;
+        private System.Windows.Forms.Label lblPlayer;
+    }
+}
+
---
+++ Lesson_7/GuessNum/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GuessNum
+{
+    public partial class mainForm : Form
+    {
+        public delegate void delPrint(string s);
+
+        public mainForm()
+        {
+            InitializeComponent();
+            InitialFormData(false);
+            formInputNumber inputNumber = new formInputNumber();
+            lblInfo.MaximumSize = new Size(this.Width - 20,0);
+        }
+
+        int max = 100; // максимальное загадываемое число
+        int count = 0; // счетчик попыток
+        int player = 1; // порядковый номер игрока
+        int uNumber; // число игрока
+        int gNumber; // загаданное число
+
+
+        private void mainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void mainForm_SizeChanged(object sender, EventArgs e)
+        {
+            lblInfo.MaximumSize = new Size(this.Width - 20, 0);
+        }
+        private void start_Click(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            int gNumber = r.Next(max + 1);
+            int count = 1;
+            int player = 1;
+            InitialFormData(true);
+            Print(new delPrint(PrintNumber), "");
+            Print(new delPrint(PrintTry), "Попытка №" + count);
+            Print(new delPrint(PrintInfo), "Число загадано, нажмите \"угадать\" для продолжения" + count);
+        }
+        // интерфейс
+        private void InitialFormData(bool flag)
+        {
+            if (flag == true)
+            {
+                lblTry.Show();
+                lblPlayer.Show();
+                number.Show();
+            }
+            else
+            {
+                lblTry.Hide();
+                lblPlayer.Hide();
+                number.Hide();
+            }
+        }
+        private void PrintInfo(string s)
+        {
+            lblInfo.Text = s;
+        }
+        private void PrintPlayer(string s)
+        {
+            lblPlayer.Text = s;
+        }
+        private void PrintTry(string s)
+        {
+            lblTry.Text = s;
+        }
+        private void PrintNumber(string s)
+        {
+            number.Text = s;
+        }
+        private void Print(delPrint P, string s)
+        {
+            P(s);
+        }
+    }
+}
