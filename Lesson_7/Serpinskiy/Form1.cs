33af8602cc442e7563888232af194967e2e11c38
maxyber <ghostpaladin@mail.ru>
2019-04-11 14:28:24 +0300

    Дописал метод Серпинского для формы с выбором прямоугольника или треугольника

---
+++ Lesson_7/Serpinskiy/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Triangle;
+using Rectangle;
+
+namespace Carpet
+{
+    public delegate void delPrint(string s);
+
+    public partial class frmCarpet : Form
+    {
+        Graphics g;
+        Random r;
+        int red, green, blue;
+
+        public frmCarpet()
+        {
+            InitializeComponent();
+            g = this.CreateGraphics();
+            r = new Random();
+            Print(new delPrint(PrintInfo), "Добро пожаловать в программу рисования по методам Серпинского");
+            rdbRectangle.Enabled = false;
+            rdbTriangle.Enabled = false;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            rdbRectangle.Enabled = true;
+            rdbTriangle.Enabled = true;
+            rdbTriangle.Checked = true;
+        }
+        //
+        // ЛОГИКА
+        private void StartDrawing(int repeater)
+        {
+            int maxX = this.Width - 10;
+            int maxY = this.Height - 45;
+            red = r.Next(256);
+            green = r.Next(256);
+            blue = r.Next(256);
+
+            if (rdbTriangle.Checked == true)
+            {
+                Triangle.Triangle uDraw = new Triangle.Triangle(10, maxX, 45, maxY);
+                int[] point = uDraw.NewPoint();
+                for (int i = 0; i < repeater; i++)
+                {
+                    int[] newpoint = uDraw.NewPoint(point);
+                    g.FillEllipse(new SolidBrush(Color.FromArgb(255, red, green, blue)), point[0], point[1], 2, 2);
+                    point = newpoint;
+                }
+            }
+            else if (rdbRectangle.Checked == true)
+            {
+                Rectangle.Rectangle uDraw = new Rectangle.Rectangle(10, maxX, 45, maxY);
+                int[] point = uDraw.NewPoint();
+                for (int i = 0; i < repeater; i++)
+                {
+                    int[] newpoint = uDraw.NewPoint(point);
+                    g.FillEllipse(new SolidBrush(Color.FromArgb(255, red, green, blue)), point[0], point[1], 2, 2);
+                    point = newpoint;
+                }
+            }
+
+            Print(new delPrint(PrintInfo), "Объект нарисован. Нажмите 'Очистить' для очистки формы и обновления точек");
+        }
+
+        private void btnStart_Click(object sender, EventArgs e) // метод обработки нажатия на кнопку Start, проверяет число повторений и по необходимости запускает рисование
+        {
+            if (int.TryParse(txtRepeater.Text, out int x) && (x <= 500000) && (x >= 10000))
+            {
+                Print(new delPrint(PrintInfo), "");
+                StartDrawing(x);
+            }
+            else Print(new delPrint(PrintAlert), "Введите, пожалуйста, число повторений не меньше 10 000 и не больше 500 000!");
+        }
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            g.FillRectangle(new SolidBrush(Color.FromArgb(this.BackColor.ToArgb())), 10, 45, this.Width - 10, this.Height - 45);
+        }
+
+        private void PrintInfo(string s)
+        {
+            lblInfo.Text = s;
+        }
+        private void PrintAlert(string s)
+        {
+            MessageBox.Show(s);
+        }
+        private void Print(delPrint P, string s)
+        {
+            P(s);
+        }
+    }
+}
---
+++ Lesson_7/Serpinskiy/Triangle/Triangle.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangle
+{
+    public class Triangle
+    {
+        int[,] points = new int[3, 2];
+        Random r = new Random();
+
+        public int[,] Points { get => points; set => points = value; }
+
+        // конструктор класса
+        public Triangle() // конструктор без параметров строит 3 нулевые точки
+        {
+            for(int i = 0;i<3;i++)
+            {
+                Points[i, 0] = 0;
+                Points[i, 1] = 0;
+            }
+        }
+        public Triangle(int maxX, int maxY) // конструктор параметрами int, int строит 3 случайных точки со значениями от 0 до max
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                Points[i, 0] = r.Next(maxX+1);
+                Points[i, 1] = r.Next(maxY+1);
+            }
+        }
+        public Triangle(int minX, int maxX, int minY, int maxY) // конструктор параметрами int, int int, int строит 3 случайных точки со значениями от min до max
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                Points[i, 0] = r.Next(minX, maxX + 1);
+                Points[i, 1] = r.Next(minY, maxY + 1);
+            }
+        }
+        public Triangle(int[,] uPoints) // конструктор с параметром int[,] принимает 3 точки массива int[3,2], либо нулевые точки, если размерность массива отличается
+        {
+            if (uPoints.Length == 6)
+            {
+                Points = uPoints;
+            }
+            else
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    Points[i, 0] = 0;
+                    Points[i, 1] = 0;
+                }
+            }
+        }
+
+        public int[] GetPoint(int index) => new int[2] { Points[index, 0], Points[index, 1] }; // возвращает массив int[2] с координатами точки index
+
+        public int Attraction() // метод выбирает случайную точку от 0 до 2
+        {
+            int index = r.Next(3);
+            return index;
+        }
+        public int[] RandomPoint() // возвращает массив int[2] с координатами случайной точки
+        {
+            int index = Attraction();
+            int[] point = this.GetPoint(index);
+            return point;
+        }
+        public int[] NewPoint() // возвращает массив int[2] с координатами первой точки запуска
+        {
+            int[] newpoint = new int[2];
+            int[] point1 = RandomPoint();
+            int[] point2 = RandomPoint();
+            while (point1 == point2)
+            {
+                point2 = RandomPoint();
+            }
+            newpoint[0] = (point1[0] + point2[0]) / 2;
+            newpoint[1] = (point1[1] + point2[1]) / 2;
+            return newpoint;
+        }
+        public int[] NewPoint(int[] oldpoint) // возвращает массив int[2] с координатами новой точки на основании предыдущей
+        {
+            int[] newpoint = new int[2];
+            int[] point1 = RandomPoint();
+            newpoint[0] = (point1[0] + oldpoint[0]) / 2;
+            newpoint[1] = (point1[1] + oldpoint[1]) / 2;
+            return newpoint;
+        }
+
+    }
+}
