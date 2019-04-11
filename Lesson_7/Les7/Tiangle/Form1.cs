14e32a090403416d9c781508daab48f020a9aa60
maxyber <ghostpaladin@mail.ru>
2019-04-11 12:25:33 +0300

    Задание 7.3 выполнено ДЗ полностью сделано.

---
+++ Lesson_7/Les7/Tiangle/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp6
+{
+    public partial class Form1 : Form
+    {
+        Graphics g;
+        Random r;
+        SolidBrush brush;
+        public Form1()
+        {
+            InitializeComponent();
+            g = this.CreateGraphics();
+            x = x1;
+            y = y1;
+            r = new Random();
+            brush = new SolidBrush(Color.Black);
+        }
+
+        int x1 = 300, y1 = 10;
+        int x2 = 10, y2 = 300;
+        int x3 = 700, y3 = 400;
+        int x, y;
+
+
+        void MovePrint(int GetX, int GetY, ref int X, ref int Y )
+        {
+            X = (X + GetX) / 2;
+            Y = (Y + GetY) / 2;
+        }
+
+
+
+
+        int red = 1, green=1, blue = 1;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+
+                switch (r.Next(3))
+                {
+
+                    case 0: MovePrint(x1, y1, ref x, ref y); break;
+                    case 1: MovePrint(x2, y2, ref x, ref y); break;
+                    default: MovePrint(x3, y3, ref x, ref y); break;
+
+                }
+
+                g.FillEllipse(new SolidBrush(Color.FromArgb(255, red, green, blue)), x, y, 5, 5);
+                red+=2; red %= 256;
+                green *= red; green %= 256;
+                // blue += 2; blue %= 256;
+            }
+
+
+        }
+    }
+}
---
+++ Lesson_7/Triangle/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace Carpet
+{
+    partial class frmCarpet
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.txtRepeater = new System.Windows.Forms.TextBox();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(700, 10);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 25);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Запуск!";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // txtRepeater
+            // 
+            this.txtRepeater.Location = new System.Drawing.Point(590, 10);
+            this.txtRepeater.MinimumSize = new System.Drawing.Size(100, 25);
+            this.txtRepeater.Name = "txtRepeater";
+            this.txtRepeater.Size = new System.Drawing.Size(100, 25);
+            this.txtRepeater.TabIndex = 1;
+            this.txtRepeater.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtRepeater.TextChanged += new System.EventHandler(this.txtRepeater_TextChanged);
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Location = new System.Drawing.Point(13, 12);
+            this.lblInfo.MaximumSize = new System.Drawing.Size(580, 0);
+            this.lblInfo.MinimumSize = new System.Drawing.Size(0, 25);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(0, 25);
+            this.lblInfo.TabIndex = 2;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(700, 45);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 25);
+            this.btnClear.TabIndex = 3;
+            this.btnClear.Text = "Очистить";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // frmCarpet
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 450);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.txtRepeater);
+            this.Controls.Add(this.btnStart);
+            this.Name = "frmCarpet";
+            this.Text = "Коврик на стенку";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox txtRepeater;
+        private System.Windows.Forms.Label lblInfo;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
